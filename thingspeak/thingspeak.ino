d819b38622d5f23ffb5173a2e9aad301d9515a5c
Sian Shinjo <sianshinjo123@gmail.com>
2025-03-27 22:55:22 +0530

    actually fixed indentation

--- thingspeak/thingspeak.ino
+++ thingspeak/thingspeak.ino
@@ -40,85 +40,86 @@ double reads2() {
 }
 
 void publishData() {
-	if (millis() - lastPostTime >= POST_INTERVAL) {
-		HTTPClient http;
-		int data = random(0, 100);
-		String url = server + "?apikey=" + apikey + "&field1=" + String(data);
-		http.begin(url);
+  if (millis() - lastPostTime >= POST_INTERVAL) {
+    HTTPClient http;
+    int data = random(0, 100);
+    String url = server + "?apikey=" + apikey + "&field1=" + String(data);
+    http.begin(url);
 
-		int httpcode = http.GET();
+    int httpcode = http.GET();
 
-		if (httpcode == 200) {
-			String payload = http.getString();
-			if (payload=="0") {
-				Serial.println("Data sent to ThingSpeak: " + String(data));
-			} else {
+    if (httpcode == 200) {
+      String payload = http.getString();
+      if (payload=="0") {
+        Serial.println("Data sent to ThingSpeak: " + String(data));
+      } else {
 				Serial.println("Error: Failed to send data to ThingSpeak.");
 			}
-		} else {
-			Serial.println("Error: HTTP request failed with error code: " + String(httpcode));
-		}
-	
-		http.end();
-		lastPostTime = millis();
+    } else {
+      Serial.println("Error: HTTP request failed with error code: " + String(httpcode));
+    }
+
+    http.end();
+    lastPostTime = millis();
+  }
 }
 
 void checkSensors() {
-	double distance1 = reads1();
-	double distance2 = reads2();
-	unsigned long current_time;
+  double distance1 = reads1();
+  double distance2 = reads2();
+  unsigned long current_time;
 
-	// sensor1 triggered at current time
-	if (distance1 < THRESHOLD && distance2 > THRESHOLD) { // sensor 1 triggered first?
-		Serial.println("S1 first");
+  // sensor1 triggered at current time
+  if (distance1 < THRESHOLD && distance2 > THRESHOLD) { // sensor 1 triggered first?
+    Serial.println("S1 first");
 
-		// try reading sensor2 for 1 second after current time
-		current_time = millis();
+    // try reading sensor2 for 1 second after current time
+    current_time = millis();
 
-		while (millis() - current_time < 1000) {
-			distance2 = reads2();
-			if (distance2 < THRESHOLD) {
-				count++;
-				Serial.println("Incremented Count: " + String(count));
-				break;
-			}
-		}
-	} else if (distance2 < THRESHOLD && distance1 > THRESHOLD) { // sensor 2 triggered first?
-		Serial.println("S2 first");
+    while (millis() - current_time < 1000) {
+      distance2 = reads2();
+      if (distance2 < THRESHOLD) {
+        count++;
+        Serial.println("Incremented Count: " + String(count));
+        break;
+      }
+    }
+  } else if (distance2 < THRESHOLD && distance1 > THRESHOLD) { // sensor 2 triggered first?
+    Serial.println("S2 first");
 
-		current_time = millis();
+    current_time = millis();
 
-		while (millis() - current_time < 1000) {
-			distance1 = reads1();
-			if (distance1 < THRESHOLD) {
-				count--;
-				Serial.println("Decremented Count: " + String(count));
-				break;
-			}
-		}
-	}
+    while (millis() - current_time < 1000) {
+      distance1 = reads1();
+      if (distance1 < THRESHOLD) {
+        count--;
+        Serial.println("Decremented Count: " + String(count));
+        break;
+      }
+    }
+  }
 }
 
 void setup() {
-	Serial.begin(9600);
-	pinMode(TRIG_PIN1, OUTPUT);
-	pinMode(ECHO_PIN1, INPUT);
-	pinMode(TRIG_PIN2, OUTPUT);
-	pinMode(ECHO_PIN2, INPUT);
+  Serial.begin(9600);
+  pinMode(TRIG_PIN1, OUTPUT);
+  pinMode(ECHO_PIN1, INPUT);
+  pinMode(TRIG_PIN2, OUTPUT);
+  pinMode(ECHO_PIN2, INPUT);
 
-	WiFi.begin(ssid, password);
-	Serial.print("Connecting to WiFi");
-	while (WiFi.status() != WL_CONNECTED) {
-		delay(500);
-		Serial.print(".");
-	}
-	Serial.println("Connected to WiFi");
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected to WiFi");
 }
 
 void loop() {
-	checkSensors();
-	publishData();
-	delay(100); // Small delay to avoid excessive pings
+  checkSensors();
+  publishData();
+  delay(100); // Small delay to avoid excessive pings
 }
 
-//
+
