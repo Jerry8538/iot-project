70057492528959b5251c7ad000068dcd3172d2fb
yk <jerr8538@gmail.com>
2025-03-27 14:35:59 +0530

    fixing inflow/outflow sensing

--- station_ultrasound/station_ultrasound.ino
+++ station_ultrasound/station_ultrasound.ino
@@ -11,53 +11,60 @@ bool sensor1_triggered = false;
 bool sensor2_triggered = false;
 unsigned long sensor1_time = 0;
 unsigned long sensor2_time = 0;
-const unsigned long RESET_TIME = 3000; // 3 seconds timeout
+const unsigned long RESET_TIME = 3000;
 
-void checkSensors() {
+double reads1() {
   digitalWrite(TRIG_PIN1, HIGH);
   delay(10);
   digitalWrite(TRIG_PIN1, LOW);
-  double distance1 = pulseIn(ECHO_PIN1, HIGH) * SOUND_SPD / 2 / 10000;
-  // Serial.println(distance1);
+  return pulseIn(ECHO_PIN1, HIGH) * SOUND_SPD / 2 / 10000;
+}
 
+double reads2() {
   digitalWrite(TRIG_PIN2, HIGH);
   delay(10);
   digitalWrite(TRIG_PIN2, LOW);
-  double distance2 = pulseIn(ECHO_PIN2, HIGH) * SOUND_SPD / 2 / 10000;
+  return pulseIn(ECHO_PIN2, HIGH) * SOUND_SPD / 2 / 10000;
+}
+
+void checkSensors() {
+  double distance1 = reads1();
+  double distance2 = reads2();
+  unsigned long current_time;
+  // Serial.println(distance1);
   // Serial.println(distance2);
-  unsigned long current_time = millis();
 
-  // Detect object within threshold
-  if (distance1 > 0 && distance1 < THRESHOLD && !sensor1_triggered) {
+  // sensor1 triggered at current time
+  if (distance1 < THRESHOLD) {
     sensor1_triggered = true;  // Mark sensor1 as triggered first
-    sensor1_time = current_time;
     Serial.println("S1 first");
-  }
-  if (distance2 > 0 && distance2 < THRESHOLD && sensor1_triggered && !sensor2_triggered) {
-    sensor2_triggered = true;  // Mark sensor2 as triggered AFTER sensor1
-    count++;
-    Serial.println("Incremented Count: " + String(count));
-    sensor1_triggered = sensor2_triggered = false;  // Reset flags
-  }
 
-  // Reverse detection (sensor2 first, then sensor1)
-  if (distance2 > 0 && distance2 < THRESHOLD && !sensor2_triggered) {
+    // try reading sensor2 for 1 second after current time
+    unsigned long current_time = millis();
+
+    while (millis() - current_time < 1000) {
+      distance2 = reads2();
+      if (distance2 < THRESHOLD) {
+        count++;
+        Serial.println("Incremented Count: " + String(count));
+        break;
+      }
+    }
+    sensor1_triggered = false;  // Reset flags
+  } else if (distance2 < THRESHOLD) {
     sensor2_triggered = true;
-    sensor2_time = current_time;
     Serial.println("S2 first");
-  }
-  if (distance1 > 0 && distance1 < THRESHOLD && sensor2_triggered && !sensor1_triggered) {
-    sensor1_triggered = true;
-    count--;
-    Serial.println("Decremented Count: " + String(count));
-    sensor1_triggered = sensor2_triggered = false;
-  }
 
-  // Reset triggers if no second trigger within 3 seconds
-  if (sensor1_triggered && (current_time - sensor1_time > RESET_TIME)) {
-    sensor1_triggered = false;
-  }
-  if (sensor2_triggered && (current_time - sensor2_time > RESET_TIME)) {
+    current_time = millis();
+
+    while (millis() - current_time < 1000) {
+      distance1 = reads1();
+      if (distance1 < THRESHOLD) {
+        count--;
+        Serial.println("Decremented Count: " + String(count));
+        break;
+      }
+    }
     sensor2_triggered = false;
   }
 }
